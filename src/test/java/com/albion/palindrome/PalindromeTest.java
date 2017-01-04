32f277162d659c788c39634ec1aa973c0665016e
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-01-03 22:30:04 -0800

    Advanced Palindrome

--- src/test/java/com/albion/palindrome/PalindromeTest.java
+++ src/test/java/com/albion/palindrome/PalindromeTest.java
@@ -1,19 +1,40 @@
 package com.albion.palindrome;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class PalindromeTest {
 
   @Test
   public void isPalindrome() {
-	  boolean result1 = Palindrome.isPalindrome(1234321);
+	  boolean result1 = Palindrome.isPalindromeNumber(1234321);
 	  Assert.assertTrue(result1);
 	  
-	  boolean result2 = Palindrome.isPalindrome(12344321);
+	  boolean result2 = Palindrome.isPalindromeNumber(12344321);
 	  Assert.assertTrue(result2);
 	  
-	  boolean result3 = Palindrome.isPalindrome(12345);
+	  boolean result3 = Palindrome.isPalindromeNumber(12345);
 	  Assert.assertFalse(result3);
+  }
+
+  @DataProvider(name = "dp01")
+  public Object[][] makeData() {
+  	return new Object[][] {
+  	        {"abcba", true},
+            {"abccba", true},
+            {"A man, a plan, a canal: Panama", true},
+            {"abca", false},
+            {"a", true},
+            {".,", false},
+            {"0P", false},
+            {"a.", true}
+	};
+  }
+  @Test(dataProvider = "dp01")
+  public void testIsPalindromeV2(String input, boolean expected) {
+	  Palindrome p = new Palindrome();
+	  boolean actual = p.isPalindromeV2(input);
+	  Assert.assertEquals(actual, expected);
   }
 }
