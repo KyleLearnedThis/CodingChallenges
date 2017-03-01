49f0832e7b83a593ca8c46d306a22ac1f7007a28
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-02-28 21:23:10 -0800

    AllPath code refactoring

--- src/main/java/com/albion/tree/allpaths/AllPaths.java
+++ src/main/java/com/albion/tree/allpaths/AllPaths.java
@@ -7,46 +7,46 @@ import java.util.List;
 
 public class AllPaths {
 	public List<String> binaryTreePaths(BinarySearchTreeNode<Integer> root) {
-	    ArrayList<String> finalResult = new ArrayList<String>();
-	 
-	    if(root==null)
-	        return finalResult;
-	 
-	    ArrayList<String> curr = new ArrayList<String>();
-	    ArrayList<ArrayList<String>> results = new ArrayList<ArrayList<String>>();
-	 
-	    dfs(root, results, curr);
-	 
-	    for(ArrayList<String> al : results){
-	        StringBuilder sb = new StringBuilder();
-	        sb.append(al.get(0));
-	        for(int i=1; i<al.size();i++){
-	            sb.append("->"+al.get(i));
-	        }
-	        String line = sb.toString();
-	        System.out.println("========= LINE: " + line);
-	        finalResult.add(line);
-	    }
-	 
-	    return finalResult;
+		ArrayList<String> finalResult = new ArrayList<>();
+
+		if(root==null) {
+			return finalResult;
+		}
+		ArrayList<String> curr = new ArrayList<>();
+		ArrayList<ArrayList<String>> results = new ArrayList<>();
+
+		dfs(root, results, curr);
+
+		for(ArrayList<String> al : results){
+			StringBuilder sb = new StringBuilder();
+			sb.append(al.get(0));
+			for(int i=1; i<al.size();i++){
+				sb.append("->"+al.get(i));
+			}
+			String line = sb.toString();
+			System.out.println("========= LINE: " + line);
+			finalResult.add(line);
+		}
+
+		return finalResult;
 	}
-	 
-	public void dfs(BinarySearchTreeNode<Integer> root, ArrayList<ArrayList<String>> list, ArrayList<String> curr){
-	    curr.add(String.valueOf(root.value));
-	 
-	    if(root.left==null && root.right==null){
-	        list.add(curr);
-	        return;
-	    }
-	 
-	    if(root.left!=null){
-	        ArrayList<String> temp = new ArrayList<String>(curr);
-	        dfs(root.getLeft(), list, temp);
-	    }
-	 
-	    if(root.right!=null){
-	        ArrayList<String> temp = new ArrayList<String>(curr);
-	        dfs(root.getRight(), list, temp);
-	    } 
+
+	public void dfs(BinarySearchTreeNode<Integer> root, ArrayList<ArrayList<String>> allPaths, ArrayList<String> path){
+		path.add(String.valueOf(root.value));
+
+		if(root.left == null && root.right == null){
+			allPaths.add(path);
+			return;
+		}
+
+		if(root.left != null){
+			ArrayList<String> temp = new ArrayList<>(path);
+			dfs(root.getLeft(), allPaths, temp);
+		}
+
+		if(root.right != null){
+			ArrayList<String> temp = new ArrayList<>(path);
+			dfs(root.getRight(), allPaths, temp);
+		}
 	}
 }
