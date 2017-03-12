20c387b55e927b36e23d1543b9f90840bea57cbf
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-03-12 16:05:22 -0700

    Redo remove link list node

--- src/main/java/com/albion/core/LinkList.java
+++ src/main/java/com/albion/core/LinkList.java
@@ -45,15 +45,34 @@ public class LinkList {
         return head;
     }
 
-    public void delete(ListNode node) {
-        if(node.next != null && node.next.next != null) {
-            if(node.next == null ){
-
-            }
-            node.val = node.next.val;
-            node.next = node.next.next;
+    public void delete(int position) {
+        // If linked list is empty
+        if (head == null) {
+            return;
         }
 
+        // Store head node
+        ListNode temp = head;
+
+        // If head needs to be removed
+        if (position == 0) {
+            head = temp.next;   // Change head
+            return;
+        }
+
+        // Find previous node of the node to be deleted
+        for (int i = 0; temp != null && i < position-1; i++) {
+            temp = temp.next;
+        }
+        // If position is more than number of nodes
+        if (temp == null || temp.next == null) {
+            return;
+        }
+        // Node temp.next is the node to be deleted
+        // Store pointer to the next of node to be deleted
+        ListNode next = temp.next.next;
+
+        temp.next = next;  // Unlink the deleted node from list
     }
 
     /**
