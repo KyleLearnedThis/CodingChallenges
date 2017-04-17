b8f0a2c051751b301ba23456e29515691c609dbe
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-04-17 13:51:46 -0700

    Minor refactoring

--- src/test/java/com/albion/classics/pascal/TriangleTest.java
+++ src/test/java/com/albion/classics/pascal/TriangleTest.java
@@ -1,15 +1,15 @@
 package com.albion.classics.pascal;
 
-import java.util.ArrayList;
-
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 public class TriangleTest {
 
   @Test
-  public void generate() {
-	  Triangle tri = new Triangle();
-	  ArrayList<ArrayList<Integer>> x = tri.generate(5);
-	  tri.print(x);
+  public void testGenerate() {
+      Triangle tri = new Triangle();
+      ArrayList<ArrayList<Integer>> x = tri.generate(5);
+      tri.print(x);
   }
 }
