f722271b40167d2c54f33d122497e8e70708face
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-04-17 00:53:03 -0700

    Move SingleElement to right package

--- src/test/java/com/albion/lists/single/SingleElementTest.java
+++ src/test/java/com/albion/lists/single/SingleElementTest.java
@@ -4,8 +4,6 @@ import com.albion.graphs.highway.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class SingleElementTest {
     @DataProvider(name = "dp01")
     public Object[][] makeData(){
@@ -14,7 +12,7 @@ public class SingleElementTest {
         int[] x2 = {3,3,7,7,10,11,11};
         int y2 = 10;
         return new Object[][] {
-//                {x1, y1},
+                {x1, y1},
                 {x2, y2}
         };
     }
