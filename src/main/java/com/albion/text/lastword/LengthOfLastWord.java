9a98604638b6e3665abdfced775532781b577969
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-04-24 20:32:35 -0700

    BinaryTreeTilt solution

--- src/main/java/com/albion/text/lastword/LengthOfLastWord.java
+++ src/main/java/com/albion/text/lastword/LengthOfLastWord.java
@@ -5,7 +5,11 @@ package com.albion.text.lastword;
  */
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
-        if(s == null || "".equals(s)) {
+        if(s == null) {
+            return 0;
+        }
+        s = s.trim();
+        if("".equals(s)) {
             return 0;
         }
         String[] words = s.split(" ");
