1952a43477d5d933896de02c40f885e82fb5cde6
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-05-10 21:03:26 -0700

    Minor refactoring, SingleElement.singleNonDuplicate fix

--- src/main/java/com/albion/lists/single/SingleElement.java
+++ src/main/java/com/albion/lists/single/SingleElement.java
@@ -5,19 +5,10 @@ package com.albion.lists.single;
  */
 public class SingleElement {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        int low = 0;
-        int high = n/2;
-        while (low < high) {
-            int m = (low + high) / 2;
-            int even = 2 * m;
-            int odd = 2 * m + 1;
-            if (nums[even] != nums[odd]) {
-                high = m;
-            } else {
-                low = m+1;
-            }
+        int x = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            x = x ^ nums[i];
         }
-        return nums[2*low];
+        return x;
     }
 }
